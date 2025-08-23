083ef66116f607b53d116aaaf24288adfa65de86
Sara <sara.rodriguez@precognis.com>
2025-08-23 09:07:54 +0200

    Proyecto base Sprint Boot

---
+++ src/main/java/com/sararoal/proyecto_tracker/ProyectoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sararoal.proyecto_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sararoal/proyecto_tracker/ProyectoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sararoal.proyecto_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
